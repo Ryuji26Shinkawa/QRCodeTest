dc9f8436488fc3bde32dc00b4105a500c1695225
Ryuji26Shinkawa <manekinke03.playgaming@gmail.com>
2024-02-10 01:22:34 +0900

    初回Push

---
+++ QRCodeTest/QRCodeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeTestApp.swift
+//  QRCodeTest
+//
+//  Created by 新川竜司 on 2024/02/10.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # QRCodeTest
-QRコードに関するテストを行うプロジェクト
+二次元コードに関するテストを行うプロジェクト
+## 参考リンク
+- 2次元コード生成
+[【SwiftUI】QRコードを生成して、中心に可愛いアイコンを付ける方法 | DevelopersIO](https://dev.classmethod.jp/articles/swift-generate-qr-code/)
+
